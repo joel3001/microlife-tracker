b19c7854be1309d1178b25887c05ac5ea6d9eba5
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-09-19 22:32:41 +0100

    createActivity test written

--- __tests__/createActivity.test.js
+++ __tests__/createActivity.test.js
@@ -2,44 +2,50 @@ const fs = require('fs');
 const path = require('path');
 const events = require('events');
 const httpMocks = require('node-mocks-http');
+
 const createActivity = require('../controllers/createActivity');
 
 jest.mock('uuid/v5', () => jest.fn(() => 'abc123'));
 
 describe('createActivity', () => {
-  it('adds an activity to the profile', (done) => {
+  it('adds an activity to the profile', done => {
+    expect.assertions(2);
+
     const request = httpMocks.createRequest({
-        method: 'POST',
-        url: '/profile/activities',
-        body: {
-          activityId: 'short-walk',
-          quantity: 1,
-        },
-        
+      method: 'POST',
+      url: '/profile/activities',
+      body: {
+        activityId: 'short-walk',
+        quantity: 1
+      }
+    });
+
+    const response = httpMocks.createResponse({
+      eventEmitter: events.EventEmitter
+    });
+
+    createActivity(request, response);
+
+    response.on('end', () => {
+      const filePath = path.join(__dirname, '../controllers', 'user.json');
+
+      fs.readFile(filePath, 'utf8', (error, userJson) => {
+        if (error) throw error;
+
+        expect(response.statusCode).toEqual(200);
+
+        const user = JSON.parse(userJson);
+        const activity = Object.assign(request.body, { _id: 'abc123' });
+
+        expect(user.profile.activities).toContainEqual(
+          expect.objectContaining(activity)
+        );
+        done();
       });
-      const response = httpMocks.createResponse({});      
+    });
   });
-
-  const response = httpMocks.createResponse({
-    eventEmitter: events.EventEmitter,
-  });
-
-  createActivity(request, response);
-
-  response.on('end', () => {
-    const filePath = path.join(__dirname, '../controllers', 'user.json');
-    fs.readFile(filePath, 'utf8', (error, userJson) => {});
-
-    expect(response.statusCode).toEqual(200);
-
-    const user = JSON.parse(userJson);
-    const activity = Object.assign(request.body, { _id: 'abc123' });
-  
-    expect(user.profile.activities).toContainEqual(expect.objectContaining(activity));
-  });
-
   afterEach(() => {
     const filePath = path.join(__dirname, '../controllers', 'user.json');
-    fs.writeFileSync(filePath, '{"profile":{"activities":[]}}');
+    fs.writeFileSync(filePath, '{"profile":{"activities": []}}');
   });
 });
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const createActivity = require('./controllers/createActivity');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.post('/profile/activities', createActivity);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
